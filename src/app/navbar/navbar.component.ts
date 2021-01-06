1a6fb46ebb847de56010cdd93e586ce58db9d9d6
Angel Guzman <angel_guzman96@hotmail.com>
2021-01-06 00:36:28 -0600

    styles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,4 +12,23 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
+  onClick(event: any) {
+    var target = event.target || event.srcElement || event.currentTarget;
+    var idAttr = target.attributes.id.nodeValue;
+    switch(idAttr){
+      case 'homeView':
+        document.getElementById('home')?.scrollIntoView({behavior:'smooth'});
+        break;
+      case 'aboutView':
+        document.getElementById('about')?.scrollIntoView({behavior:'smooth'});
+        break;
+      case 'serviceView': 
+        document.getElementById('service')?.scrollIntoView({behavior:'smooth'});
+        break;
+      case 'contactView':
+        document.getElementById('contact')?.scrollIntoView({behavior:'smooth'});
+        break;
+    }
+  }
 }
